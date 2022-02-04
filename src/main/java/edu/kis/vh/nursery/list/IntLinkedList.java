fb6bc7d6e5dabb1214fda4030468f67108f99f4d
Jakub Mościcki <225344@edu.p.lodz.pl>
2022-02-04 23:35:49 +0100

    1.5.6 - Dokumentacja.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,17 @@
 package edu.kis.vh.nursery.list;
-
+/**
+ * Klasa przetrzymuje listę dwukierunkową wartości liczb całkowitych
+ */
 public class IntLinkedList {
 
 	public static final int FAIL = -1;
-	Node last;
+	private Node last;
 	int i;
+	
+	/**
+	 *i: wartość nowego Noda, która zostanie dodana do listy
+	 * Funkcja dodaje element do listy
+	 */
 
 	public void push(int i) {
 		if (getLast() == null)
@@ -16,20 +23,29 @@ public class IntLinkedList {
 		}
 	}
 
+	/**
+	 * Zwraca wartość true lub false w zależności od tego czy posiada element Node przypisany do pola last
+	 */
 	public boolean isEmpty() {
 		return getLast() == null;
 	}
-
+	/**
+	 * Zawsze zwraca wartość false, funkcja sprawdza, czy lista jest pełna
+	 */
 	public boolean isFull() {
 		return false;
 	}
-
+	/**
+	 * Zwraca wartość ostatniego Node w liście, jeżeli lista jest pusta zwraca kod błędu
+	 */
 	public int top() {
 		if (isEmpty())
 			return FAIL;
 		return getLast().getValue();
 	}
-
+	/**
+	 * Usuwa ostatni element z list, gdy lusta, jeżeli lista jest pusta zwraca kod błędu
+	 */
 	public int pop() {
 		if (isEmpty())
 			return FAIL;
@@ -37,10 +53,17 @@ public class IntLinkedList {
 		setLast(getLast().getPrev());
 		return ret;
 	}
+	/**
+	 * Zwraca ostatni Node
+	 */
 	public Node getLast() {
 		return last;
 	}
 
+	/**
+	 * last: Node do dodania na koniec listy
+	 * Dodaje element do końca listy
+	 */
 	public void setLast(Node last) {
 		this.last = last;
 	}
